b2ec9d6319abcd56ec2be8e087772f5b9cf416df
Rebecca Panisset <rebeccabarreto10@gmail.com>
2024-02-15 20:53:00 -0300

    project codebase

---
+++ src/main/java/br/com/numsei/pagepals/PagepalsApplication.java
@@ -0,0 +1,13 @@
+package br.com.numsei.pagepals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagepalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagepalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/numsei/pagepals/PagepalsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.numsei.pagepals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagepalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
